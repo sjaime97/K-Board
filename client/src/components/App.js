c3b85c09a23169560e06bcd8ba57123cedd4848b
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-11-13 14:39:28 -0800

    Imported reducer into app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import TrelloList from "./TrelloList";
-
+import { connect } from "react-redux";
 
 class App extends Component {
-  render(){
+  render() {
+    const { lists } = this.props;
     return (
       <div className="App">
         <h1>K-Board</h1>
-        <TrelloList title="Tasks"/>
+        <div style={styles.listContainer}>
+          {lists.map((list) => (
+            <TrelloList title={list.title} cards={list.cards} />
+          ))}
+        </div>
       </div>
     );
   }
-}  
+}
 
-export default App;
+const styles = {
+  listContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginRight: 8,
+  },
+};
+
+const mapStateToProps = (state) => ({
+  lists: state.lists,
+});
+
+export default connect(mapStateToProps)(App);
