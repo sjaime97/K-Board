52ca711a041e5753ac6c89d8356768b6d6ab71db
Nathan <abegaznathan@gmail.com>
2020-11-14 17:53:42 -0800

    Added DRAG_HAPPENED constant

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,9 +2,7 @@ export * from "./listsActions";
 export * from "./cardsActions";
 
 export const CONSTANTS = {
-    ADD_CARD: "ADD_CARD",
-    ADD_LIST: "ADD_LIST"
+  ADD_CARD: "ADD_CARD",
+  ADD_LIST: "ADD_LIST",
+  DRAG_HAPPENED: "DRAG_HAPPENED",
 };
-
-
-
