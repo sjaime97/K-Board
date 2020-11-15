0ca3abe1786d1e75e771fef62cd6f0db506cee45
Nathan <abegaznathan@gmail.com>
2020-11-14 17:17:15 -0800

    Added DragDropContext

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,24 +2,31 @@ import React, { Component } from "react";
 import TrelloList from "./TrelloList";
 import { connect } from "react-redux";
 import TrelloActionButton from "./TrelloActionButton";
+import { DragDropContext } from "react-beautiful-dnd";
 class App extends Component {
+  onDragEnd = () => {
+    //Todo: reording logic
+  };
+
   render() {
     const { lists } = this.props;
     return (
-      <div className="App">
-        <h1>K-Board</h1>
-        <div style={styles.listContainer}>
-          {lists.map((list) => (
-            <TrelloList
-              listID={list.id}
-              key={list.id}
-              title={list.title}
-              cards={list.cards}
-            />
-          ))}
-          <TrelloActionButton list />
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div>
+          <h1>K-Board</h1>
+          <div style={styles.listContainer}>
+            {lists.map((list) => (
+              <TrelloList
+                listID={list.id}
+                key={list.id}
+                title={list.title}
+                cards={list.cards}
+              />
+            ))}
+            <TrelloActionButton list />
+          </div>
         </div>
-      </div>
+      </DragDropContext>
     );
   }
 }
