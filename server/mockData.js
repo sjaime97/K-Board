5f5c7f1802607743201c36c88cc42ec6d1779625
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-11-15 13:23:49 -0800

    Added mockData.js

---
+++ server/mockData.js
@@ -0,0 +1,82 @@
+const mockDatabase = {
+  userID1: {
+    username: "jon",
+    userBoards: [
+      {
+        boardID: "hash1",
+        boardName: "hello",
+        boardData: [
+          {
+            title: "First List",
+            id: "list-ed36c6d0-1a0a-45ba-ad66-e1dc60e05aee",
+            cards: [
+              {
+                id: "card-e35dbcb9-3a50-4fb4-a7a4-e4f7bf20640b",
+                text: "We created a static list and a static card",
+              },
+              {
+                id: "card-deda46a0-fc7e-4836-a9d8-430f14ea404b",
+                text: "Card 2 text here",
+              },
+            ],
+          },
+          {
+            title: "Second list",
+            id: "list-045ecfa1-b33b-4b08-9074-82712278035e",
+            cards: [
+              {
+                id: "card-8818344b-6d0a-4a12-8fe3-d1d63a72c88a",
+                text: "Card 1 text here",
+              },
+              {
+                id: "card-f36332d0-0a26-4dc3-bfa1-589f5d4947d4",
+                text: "Card 2 text here",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  userID2: {
+    username: "Mary",
+    userBoards: [
+      {
+        boardID: "hash1",
+        boardName: "hello",
+        boardData: [
+          {
+            title: "First List",
+            id: "list-ed36c6d0-1a0a-45ba-ad66-e1dc60e05aee",
+            cards: [
+              {
+                id: "card-e35dbcb9-3a50-4fb4-a7a4-e4f7bf20640b",
+                text: "We created a static list and a static card",
+              },
+              {
+                id: "card-deda46a0-fc7e-4836-a9d8-430f14ea404b",
+                text: "Card 2 text here",
+              },
+            ],
+          },
+          {
+            title: "Second list",
+            id: "list-045ecfa1-b33b-4b08-9074-82712278035e",
+            cards: [
+              {
+                id: "card-8818344b-6d0a-4a12-8fe3-d1d63a72c88a",
+                text: "Card 1 text here",
+              },
+              {
+                id: "card-f36332d0-0a26-4dc3-bfa1-589f5d4947d4",
+                text: "Card 2 text here",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = mockDatabase;
