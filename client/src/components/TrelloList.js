5d5d88dba4b004c363124f3c4925d858954d766c
Nathan <abegaznathan@gmail.com>
2020-11-14 17:16:41 -0800

    Added droppable

--- client/src/components/TrelloList.js
+++ client/src/components/TrelloList.js
@@ -1,17 +1,26 @@
 import React from "react";
 import TrelloCard from "./TrelloCard";
 import TrelloActionButton from "./TrelloActionButton";
-
+import { Droppable } from "react-beautiful-dnd";
 const TrelloList = (props) => {
   const { title, cards, listID } = props;
   return (
-    <div style={styles.container}>
-      <h4>{title}</h4>
-      {cards.map((card) => (
-        <TrelloCard key={card.id} text={card.text} />
-      ))}
-      <TrelloActionButton listID={listID} />
-    </div>
+    <Droppable droppableId={String(listID)}>
+      {(provided) => (
+        <div
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          style={styles.container}
+        >
+          <h4>{title}</h4>
+          {cards.map((card, index) => (
+            <TrelloCard key={card.id} text={card.text} id={card.id} index={index}/>
+          ))}
+          <TrelloActionButton listID={listID} />
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   );
 };
 
@@ -22,7 +31,7 @@ const styles = {
     width: 300,
     padding: 8,
     marginRight: 8,
-    height: "100%"
+    height: "100%",
   },
 };
 
