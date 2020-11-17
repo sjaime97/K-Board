419564c3db5a8bd5fc5b00dfddc2850c1b482afa
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-11-17 14:33:20 -0800

    Connected express app to dynamodb

--- server/dynamoDB.js
+++ server/dynamoDB.js
@@ -1,3 +1,4 @@
+const { AlexaForBusiness } = require("aws-sdk");
 const AWS = require("aws-sdk");
 
 AWS.config.update({
@@ -8,14 +9,113 @@ AWS.config.update({
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 async function getBoard(userID, boardID) {
-  // return docClient.get()
+  const params = {
+    TableName: "UserBoards",
+    Key: {
+      userID: userID,
+    },
+  };
+
+  try {
+    // Query the "UserBoards" table to get user data
+    const data = await docClient.get(params).promise();
+    const { Item } = data;
+    const list = Item.userBoards;
+
+    // Iterate through the user's board list and find the
+    // target board
+    let curr;
+    for (let i = 0; i < list.length; i++) {
+      curr = list[i];
+      if (curr.boardID === boardID) {
+        return curr;
+      }
+    }
+
+    return undefined;
+  } catch (error) {
+    console.log(
+      "Unable to read item. Error JSON:",
+      JSON.stringify(error, null, 2)
+    );
+    return undefined;
+  }
 }
 
-async function getListOfBoards(userID) {
-  // return docClient.get()
+async function getListOfBoardNames(userID) {
+  const params = {
+    TableName: "UserBoards",
+    Key: {
+      userID: userID,
+    },
+  };
+
+  try {
+    // Query the "UserBoards" table to get user data
+    const data = await docClient.get(params).promise();
+    const { Item } = data;
+    const userBoards = Item.userBoards;
+
+    const listOfBoardNames = userBoards.map((board) => ({
+      boardID: board.boardID,
+      boardName: board.boardName,
+    }));
+
+    return listOfBoardNames;
+  } catch (error) {
+    console.log(
+      "Unable to read item. Error JSON:",
+      JSON.stringify(error, null, 2)
+    );
+    return undefined;
+  }
+}
+
+async function updateBoardData(userID, boardID, newData) {
+  let params = {
+    TableName: "UserBoards",
+    Key: {
+      userID: userID,
+    },
+  };
+
+  try {
+    // Query the "UserBoards" table to get user data
+    const data = await docClient.get(params).promise();
+    const { Item } = data;
+    const userBoards = Item.userBoards;
+
+    // Iterate through the user's board list and find the
+    // target board
+    let curr;
+    for (let i = 0; i < userBoards.length; i++) {
+      curr = userBoards[i];
+      if (curr.boardID === boardID) {
+        curr.boardData = newData;
+        break;
+      } else if (i === userBoards.length - 1) {
+        return "error";
+      }
+    }
+
+    params = {
+      TableName: "UserBoards",
+      Item: Item,
+    };
+    const response = await docClient.put(params).promise();
+
+    return "success";
+  } catch (error) {
+    console.log(
+      "Unable to read item. Error JSON:",
+      JSON.stringify(error, null, 2)
+    );
+    return "error";
+  }
 }
 
 module.exports = {
   getBoard,
-  getListOfBoards,
+  getListOfBoardNames,
+  updateBoardData,
 };
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require("cors");
-const mockDatabase = require("./mockData");
 const bodyParser = require("body-parser");
 const dbClient = require("./dynamoDB");
 
@@ -10,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Returns a list of boards given a userID
-app.get("/boards", (req, res) => {
+app.get("/boards", async (req, res) => {
   const { userID } = req.query;
 
   // Return error code if no userID was provided
@@ -18,27 +17,19 @@ app.get("/boards", (req, res) => {
     return res.status(400);
   }
 
-  // Get user data from database
-  const userData = mockDatabase[userID];
-  if (!userData) {
-    console.log("User: ", userID, " DNE");
-    return res.sendStatus(404);
+  // Get list of board names
+  const listOfBoardNames = await dbClient.getListOfBoardNames(userID);
+  if (!listOfBoardNames) {
+    return res.status(404).send(`User id ${userID} does not have any boards`);
   }
 
-  // Build list of boards = [{boardsID: hash#1, boardName: 'hello'}]
-  const listOfBoards = userData.userBoards.map((board) => ({
-    boardID: board.boardID,
-    boardName: board.boardName,
-  }));
-  console.log(listOfBoards);
-
   return res.status(200).send({
-    boards: listOfBoards,
+    boards: listOfBoardNames,
   });
 });
 
 // Saves a board given the userID, boardID, and new data
-app.post("/board", (req, res) => {
+app.put("/board", async (req, res) => {
   const { userID, boardID, data } = req.body;
 
   // If any of them are undefined
@@ -46,14 +37,21 @@ app.post("/board", (req, res) => {
     return res.sendStatus(400);
   }
 
-  // TODO: Query DynamoDB to save new data
+  const response = await dbClient.updateBoardData(userID, boardID, data);
+  if (response === "error") {
+    return res
+      .status(404)
+      .send(
+        `Could not update data for board id: ${boardID} and user id: ${userID}`
+      );
+  }
 
   // If succeeded, return 200
   return res.sendStatus(200);
 });
 
 // Returns 1 board given the userID and boardID
-app.get("/board", (req, res) => {
+app.get("/board", async (req, res) => {
   const { userID, boardID } = req.query;
 
   // Return error code if userID or boardID wasn't provided
@@ -61,26 +59,14 @@ app.get("/board", (req, res) => {
     return res.sendStatus(400);
   }
 
-  // Get user data from db
-  const userData = mockDatabase[userID];
-  if (!userData) {
-    console.log("User: ", userID, " DNE");
-    return res.sendStatus(404);
+  const board = await dbClient.getBoard(userID, boardID);
+  if (!board) {
+    return res
+      .status(404)
+      .send(`Board with id ${boardID} and user id ${userID} DNE`);
   }
 
-  // Iterate through board list and return data of board desired (by boardID)
-  const boards = userData.userBoards;
-  for (let i = 0; i < boards.length; i++) {
-    const board = boards[i];
-    if (board.boardID === boardID) {
-      return res
-        .status(200)
-        .send({ boardTitle: board.boardName, data: board.boardData });
-    }
-  }
-
-  // If not found, return 404
-  return res.sendStatus(404);
+  return res.send(board);
 });
 
 app.listen(port, () => {
