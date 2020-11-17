3e6ae4545c47bf02b9be4c4c73529ea8027244ce
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-11-17 09:51:57 -0800

    Moved all App.js code to new component called TrelloBoard.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,68 +1,16 @@
-import React, { Component } from "react";
-import TrelloList from "./TrelloList";
-import { connect } from "react-redux";
-import TrelloActionButton from "./TrelloActionButton";
-import { DragDropContext } from "react-beautiful-dnd";
-import { sort } from "../actions";
-//import AuthForm from "./AuthForm";
+import React from "react";
+import TrelloBoard from "./TrelloBoard";
+import NavBar from "./NavBar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-class App extends Component {
-  onDragEnd = (result) => {
-    const { destination, source, draggableId } = result;
-
-    // If destination ended up at a non-droppable location
-    if (!destination) {
-      return;
-    }
-
-    this.props.dispatch(
-      sort(
-        source.droppableId,
-        destination.droppableId,
-        source.index,
-        destination.index,
-        draggableId
-      )
-    );
-  };
-
-//   testAuthForm = () => {
-//     return <AuthForm />;
-//   };
-
-  render() {
-    const { lists } = this.props;
-    return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <div>
-          <h1>K-Board</h1>
-          <div style={styles.listContainer}>
-            {lists.map((list) => (
-              <TrelloList
-                listID={list.id}
-                key={list.id}
-                title={list.title}
-                cards={list.cards}
-              />
-            ))}
-            <TrelloActionButton list />
-          </div>
-        </div>
-      </DragDropContext>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* <NavBar /> */}
+        {/* <TrelloBoard /> */}
+        {/* <Route component={HomePage} exact path="/" /> */}
+      </Switch>
+    </BrowserRouter>
+  );
 }
-
-const styles = {
-  listContainer: {
-    display: "flex",
-    flexDirection: "row",
-    marginRight: 8,
-  },
-};
-
-const mapStateToProps = (state) => ({
-  lists: state.lists,
-});
-
-export default connect(mapStateToProps)(App);
---
+++ client/src/components/TrelloBoard.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import TrelloList from "./TrelloList";
+import { connect } from "react-redux";
+import TrelloActionButton from "./TrelloActionButton";
+import { DragDropContext } from "react-beautiful-dnd";
+import { sort } from "../actions";
+//import AuthForm from "./AuthForm";
+
+class TrelloBoard extends Component {
+  onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    // If destination ended up at a non-droppable location
+    if (!destination) {
+      return;
+    }
+
+    this.props.dispatch(
+      sort(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId
+      )
+    );
+  };
+
+  //   testAuthForm = () => {
+  //     return <AuthForm />;
+  //   };
+
+  render() {
+    const { lists } = this.props;
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div>
+          <h1>K-Board</h1>
+          <div style={styles.listContainer}>
+            {lists.map((list) => (
+              <TrelloList
+                listID={list.id}
+                key={list.id}
+                title={list.title}
+                cards={list.cards}
+              />
+            ))}
+            <TrelloActionButton list />
+          </div>
+        </div>
+      </DragDropContext>
+    );
+  }
+}
+
+const styles = {
+  listContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginRight: 8,
+  },
+};
+
+const mapStateToProps = (state) => ({
+  lists: state.lists,
+});
+
+export default connect(mapStateToProps)(TrelloBoard);
